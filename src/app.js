552db645558a08213ff3914f7f9eed3a2b4f2982
Steven Truong <49035567+spttruong@users.noreply.github.com>
2021-05-23 21:42:27 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ export { default as Favorites } from './Favorites.js';
 export const axios = require('axios');
 
 export const config = {
-  api: 'http://my-json-server.typicode.com/spttruong/vuemywardrobe-api',
+  api: 'https://my-json-server.typicode.com/spttruong/vuemywardrobe-api/',
 };
